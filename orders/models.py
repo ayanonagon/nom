5e27e76ff015b9d287793dd711dae82e4d73068b
Ayaka Nonaka <ayanonagon@gmail.com>
2012-03-31 17:36:58 -0400

    added preliminary order model

--- orders/models.py
+++ orders/models.py
@@ -4,7 +4,12 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     """A Django model representing a user."""
     phone_number = models.CharField(max_length=11) 
-    user = models.ForeignKey(User, unique=True)
+    user = models.OneToOneField(User)
     
     def __unicode__(self):
         return self.phone_number
+
+class Order(models.Model):
+    """A Django model representing a food order."""
+    owner = model.OneToOneField(UserProfile) # The user who started the order.
+    joiners = model.ManyToManyField(UserProfile) # Other users who want in on the order.
