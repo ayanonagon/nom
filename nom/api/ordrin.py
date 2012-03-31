9e4a9567d3aa7205c929fb44109d8e87a18e043c
Ayaka Nonaka <ayanonagon@gmail.com>
2012-03-31 19:47:58 -0400

    added initial ordrin code if we are going to use it

---
+++ nom/api/ordrin.py
@@ -0,0 +1,12 @@
+import urllib
+import urllib2
+
+ORDERIN_API_KEY = 'X2RlbGl2ZXJpbmciOjAsIg'
+TEST_URL = 'https://r-test.ordr.in/'
+
+def get_restaurants(street, city, zipcode):
+    url = (TEST_URL + 'dl/ASAP/' + zipcode + '/' + city + '/' + street).replace(' ', '+')
+    return urllib2.urlopen(url).read()
+
+if __name__ == '__main__':
+    print get_restaurants('3913 Baltimore Avenue', 'Philadelphia', '19104')
