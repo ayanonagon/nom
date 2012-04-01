fa4ba8489302be6f0a0e5cd89a13bed0dc932391
Ayaka Nonaka <ayanonagon@gmail.com>
2012-04-01 16:18:36 -0400

    fixed users / orders schema

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-     
+
 class UserProfile(models.Model):
     """A Django model representing a user."""
     phone_number = models.CharField(max_length=11) 
@@ -11,5 +11,5 @@ class UserProfile(models.Model):
 
 class Order(models.Model):
     """A Django model representing a food order."""
-    owner = models.OneToOneField(UserProfile) # The user who started the order.
-    # joiners = models.ManyToManyField(UserProfile) # Other users who want in on the order.
+    owner = models.ForeignKey(UserProfile, related_name="started_orders") # The user who started the order.
+    joiners = models.ManyToManyField(UserProfile, related_name="joined_orders") # Other users who want in on the order.
