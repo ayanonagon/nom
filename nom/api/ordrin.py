8856684df4a58b034ef02211e74bfda715b2a91f
Ayaka Nonaka <ayanonagon@gmail.com>
2012-04-16 12:37:11 -0400

    added foundation

--- nom/api/ordrin.py
+++ nom/api/ordrin.py
@@ -5,10 +5,23 @@ ORDERIN_API_KEY = 'X2RlbGl2ZXJpbmciOjAsIg'
 TEST_URL = 'https://r-test.ordr.in/'
 
 def get_restaurants(street, city, zipcode):
+    """Return a list of JSON objects representing restaurants around a given
+    street, city, and zipcode."""
     url = (TEST_URL + 'dl/ASAP/' + zipcode + '/' + city + '/' + street).replace(' ', '+')
     return simplejson.loads(urllib2.urlopen(url).read())
+
+def get_menu_items(restaurant_id):
+    """Return a list of JSON objects representing menu items for a restaurant
+    identified by the give id."""
+    url = TEST_URL + 'rd/' + str(restaurant_id)
+    response = urllib2.urlopen(url).read()
+    return simplejson.loads(response)['menu']
 
 if __name__ == '__main__':
     restaurants = get_restaurants('401 Harvey Road', 'College Station', '77840')
     for restaurant in restaurants:
         print restaurant
+    
+    items = get_menu_items(100)
+    for item in items:
+        print item
