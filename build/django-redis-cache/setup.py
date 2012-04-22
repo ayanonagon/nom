63fc0fb59cafbb830b043957a5aeb0134b459b29
Amalia Hawkins <amopet4@gmail.com>
2012-04-21 23:56:49 -0400

    ...

---
+++ build/django-redis-cache/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name = "django-redis-cache",
+    url = "http://github.com/sebleier/django-redis-cache/",
+    author = "Sean Bleier",
+    author_email = "sebleier@gmail.com",
+    version = "0.9.2",
+    packages = ["redis_cache"],
+    description = "Redis Cache Backend for Django",
+    install_requires=['redis>=2.4.5',],
+    classifiers = [
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+    ],
+)
