13b7d2eb8b5c2f501e9d439f9baf401781c25f1d
Amalia Hawkins <amopet4@gmail.com>
2012-04-21 22:35:10 -0400

    testing like a boss

--- nom/urls.py
+++ nom/urls.py
@@ -7,8 +7,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'nom.views.home', name='home'),
-    # url(r'^nom/', include('nom.foo.urls')),
+    url(r'^$', 'nom.views.home', name='home'),
+    # url(r'^nom/', include('nom.orders.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
