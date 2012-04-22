727f8848dac97f1376cc62d7949782ea9ed9db07
Amalia Hawkins <amopet4@gmail.com>
2012-04-21 22:43:43 -0400

    messing around

--- orders/views.py
+++ orders/views.py
@@ -1,1 +1,6 @@
+from django.shortcuts import render
+from app.models import UserProfile
 
+def index(request):
+    userprofiles = UserProfile.objects.all()
+    return render(request, 'index.html', {'userprofiles': userprofiles})
