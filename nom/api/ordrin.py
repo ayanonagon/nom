4f77ca69c2d4088a398b5081437befecfab76996
Pulak Mittal <pulak1@gmail.com>
2012-04-26 21:28:37 -0400

    Added get_restaurants() method

--- nom/api/ordrin.py
+++ nom/api/ordrin.py
@@ -5,7 +5,7 @@ from food import Restaurant
 ORDERIN_API_KEY = 'X2RlbGl2ZXJpbmciOjAsIg'
 TEST_URL = 'https://r-test.ordr.in/'
 
-def get_restaurants(street, city, zipcode):
+def __find_restaurants(street, city, zipcode):
     """Return a list of JSON objects representing restaurants around a given
     street, city, and zipcode."""
     url = (TEST_URL + 'dl/ASAP/' + zipcode + '/' + city + '/' + street).replace(' ', '+')
@@ -22,12 +22,21 @@ def turn_into_restaurant(res):
     return Restaurant(res['city'], res['ad'], res['mino'], res['na'], res['del'], 
             res['is_delivering'], res['id'], res['cu'])
 
-if __name__ == '__main__':
-    restaurants = get_restaurants('401 Harvey Road', 'College Station', '77840')
+
+def get_restaurants(street, city, zipcode):
+    restaurants = find_restaurants('401 Harvey Road', 'College Station', '77840')
+    rests = []
     for restaurant in restaurants:
-        rest = turn_into_restaurant(restaurant)
+        rests.append(turn_into_restaurant(restaurant))
+    return rests
+    
+
+if __name__ == '__main__':
+    restaurants = find_restaurants('401 Harvey Road', 'College Station', '77840')
+    for restaurant in restaurants:
+        #rest = turn_into_restaurant(restaurant)
+        pass
     
     items = get_menu_items(100)
     for item in items:
-        #print item
-        pass
+        print item
