8033e1fc18085ee9ee400d583c01abc9a9e22894
Ayaka Nonaka <ayanonagon@gmail.com>
2012-04-27 00:41:03 -0400

    added but removed ordrin ordering logic. terrible api.

--- nom/api/ordrin.py
+++ nom/api/ordrin.py
@@ -3,18 +3,19 @@ import simplejson
 from food import Restaurant
 
 ORDERIN_API_KEY = 'X2RlbGl2ZXJpbmciOjAsIg'
-TEST_URL = 'https://r-test.ordr.in/'
+TEST_URL_R = 'https://r-test.ordr.in/'
+TEST_URL_O = 'https://o-test.ordr.in/'
 
 def __find_restaurants(street, city, zipcode):
     """Return a list of JSON objects representing restaurants around a given
     street, city, and zipcode."""
-    url = (TEST_URL + 'dl/ASAP/' + zipcode + '/' + city + '/' + street).replace(' ', '+')
+    url = (TEST_URL_R + 'dl/ASAP/' + zipcode + '/' + city + '/' + street).replace(' ', '+')
     return simplejson.loads(urllib2.urlopen(url).read())
 
 def get_menu_items(restaurant_id):
     """Return a list of JSON objects representing menu items for a restaurant
     identified by the give id."""
-    url = TEST_URL + 'rd/' + str(restaurant_id)
+    url = TEST_URL_R + 'rd/' + str(restaurant_id)
     response = urllib2.urlopen(url).read()
     return simplejson.loads(response)['menu']
 
@@ -28,7 +29,7 @@ def get_restaurants(street, city, zipcode):
     return [turn_into_restaurant(restaurant) for restaurant in restaurants]
 
 if __name__ == '__main__':
-    restaurants = find_restaurants('401 Harvey Road', 'College Station', '77840')
+    restaurants = __find_restaurants('401 Harvey Road', 'College Station', '77840')
     for restaurant in restaurants:
         #rest = turn_into_restaurant(restaurant)
         pass
