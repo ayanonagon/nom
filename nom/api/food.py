df61ec2d5a28de18ef45b2ad25b77a34bc14d17e
Ayaka Nonaka <ayanonagon@gmail.com>
2012-04-27 03:01:11 -0400

    added documentation

--- nom/api/food.py
+++ nom/api/food.py
@@ -1,6 +1,8 @@
 """Contains class information for Restaurants and Items"""
 
 class Restaurant(object):
+    """A class representing a Ordrin restaurant."""
+
     def __init__(self, city, address, min_order, name, delivery_time, 
             is_delivering, rid, tags):
         self._city = city
@@ -16,24 +18,31 @@ class Restaurant(object):
     @property
     def city(self):
         return self._city
+
     @property
     def address(self):
         return self._address
+
     @property
     def min_order(self):
         return self._min_order
+
     @property
     def name(self):
         return self._name
+
     @property
     def delivery_time(self):
         return self._delivery_time
+
     @property
     def is_delivering(self):
         return self._is_delivering
+
     @property
     def rid(self):
         return self._rid
+
     @property
     def tags(self):
         return self._tags
@@ -45,20 +54,23 @@ class Item(object):
         self._description = description
         self._iid = iid
         self._children = children
-        
 
     @property
     def name(self):
         return self._name
+
     @property
     def price(self):
         return self._price
+
     @property
     def description(self):
         return self._description
+
     @property
     def iid(self):
         return self._iid
+
     @property
     def children(self):
         return self._children
@@ -66,8 +78,11 @@ class Item(object):
 
 
 class Category(object):
+    """A class representing a Ordrin food category."""
+
     def __init__(self, name):
         self._name = name
+
     @property
     def name(self):
         return self._name
